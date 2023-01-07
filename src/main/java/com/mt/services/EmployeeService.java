a321d5b8e83d4a8a18e8ccccc2a7dbeb51ff1ca7
sapershome1 <120657379+sapershome1@users.noreply.github.com>
2023-01-07 22:15:45 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,11 +22,11 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
-		js.put("Places he like", "His native place");
+		js.put("Name", "Admin");
+		js.put("Calling Name", "user");
+		js.put("DOB", "01-Jan-2000");
+		js.put("Hobbies", "Reading");
+		js.put("Places he like", "Nature");
 
 		return js.toString();
 }
